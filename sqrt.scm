12ef54bc49b05f75b37cfc673940d03b1b9f1ef7
zxymike <zxymike93@gmail.com>
2017-07-24 00:20:17 +0800

    1.1

---
+++ sqrt.scm
@@ -0,0 +1,14 @@
+(define (sqrt x)
+    (define (average a b)
+        (/ (+ a b) 2))
+    (define (good-enough? guess)
+        (< (abs (- (square guess) x)) 0.001))
+    (define (improve guess)
+        (average guess (/ x guess)))
+    (define (sqrt-iter guess)
+        (if (good-enough? guess)
+            guess
+            (sqrt-iter (improve guess))))
+    (sqrt-iter 1.0))
+
+(sqrt 2)
