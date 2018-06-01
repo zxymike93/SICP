dc8f61b6f3d2ab537c889935be8a68f252abc535
zxymike <zxymike93@gmail.com>
2018-06-01 16:58:25 +0800

    Linear Recursion and Iteration

---
+++ factorial-iteration.scm
@@ -0,0 +1,9 @@
+(define (factorial n)
+    (fact-iter 1 1 n)
+)
+
+(define (fact-iter product counter max-count)
+    (if (> counter max-count) product
+        (fact-iter (* counter product) (+ counter 1) max-count)
+    )
+)
---
+++ factorial-recursion.scm
@@ -0,0 +1,5 @@
+(define (factorial n)
+    (if (= n 1) 1
+        (* n (factorial (- n 1)))
+    )
+)
